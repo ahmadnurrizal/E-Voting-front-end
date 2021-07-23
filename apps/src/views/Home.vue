c8620a64995a874b5c60f93e0644ac988e9864bd
rizkinabil <rizki.nbl123@gmail.com>
2021-07-23 23:59:46 +0700

    add new component

--- apps/src/views/Home.vue
+++ apps/src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
    <div class="body">
-      <Navbar v-if="isLogin" />
-      <!-- <Navbar2 v-else="isLogin" /> -->
-      <Hero />
+      <NavLog v-if="isLogin" />
+      <Navbar v-else />
+      <Hero v-if="isLogin" />
+      <notLogged v-else />
       <Poll />
-      <About-poll />
+      <About-poll v-if="isLogin" />
+      <notAbt v-else />
       <Features />
       <Footer />
    </div>
@@ -13,11 +15,15 @@
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import NavLog from "@/components/NavLog.vue";
 import Hero from "@/components/Herosection.vue";
 import Poll from "@/components/Poll.vue";
 import AboutPoll from "../components/About-poll.vue";
 import Features from "../components/Features.vue";
 import Footer from "../components/Footer.vue";
+
+import notLogged from "../components/notlogHero.vue";
+import notAbt from "../components/notlogAbt.vue";
 
 import axios from "axios";
 
@@ -25,11 +31,14 @@ export default {
    name: "Home",
    components: {
       Navbar,
+      NavLog,
       Hero,
       Poll,
       AboutPoll,
       Features,
       Footer,
+      notLogged,
+      notAbt,
    },
    data() {
       return {
