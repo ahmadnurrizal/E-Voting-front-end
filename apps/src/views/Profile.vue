30f84033a2783c321188f4bbc169c2370d220195
rizkinabil <rizki.nbl123@gmail.com>
2021-07-25 00:33:14 +0700

    adding profile view

---
+++ apps/src/views/Profile.vue
@@ -0,0 +1,49 @@
+<template>
+   <div class="wrapper">
+      <NavLog />
+      <div class="wave-top"><div class="vec"></div></div>
+      <div class="main">
+         <h1>Edit Profile</h1>
+      </div>
+      <Footer />
+   </div>
+</template>
+
+<script>
+import NavLog from "@/components/NavLog.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+   name: "Profile",
+   components: {
+      NavLog,
+      Footer,
+   },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Kanit:wght@500;600&family=Roboto&display=swap");
+
+* {
+   padding: 0;
+   margin: 0;
+   box-sizing: border-box;
+}
+
+.wave-top {
+   padding-top: 60px;
+}
+
+.vec {
+   padding-top: 200px;
+   padding-bottom: 170px;
+   background-image: url("../../public/img/vector/signup-wave.svg");
+   background-size: cover;
+   background-repeat: no-repeat;
+}
+
+.wrapper {
+   background-color: #eaf5ff;
+}
+</style>
