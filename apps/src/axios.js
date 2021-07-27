c757ea0b0cbe6426f1271cce8b495af7966c11da
rizkinabil <rizki.nbl123@gmail.com>
2021-07-27 21:50:46 +0700

    default Auth config

--- apps/src/axios.js
+++ apps/src/axios.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "http://finger-vote-2021.herokuapp.com/";
+
+axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("token");
