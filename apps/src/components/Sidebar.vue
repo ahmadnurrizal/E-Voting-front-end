6bd66454e2e58e91b2cdf731af2823881b449897
rizkinabil <rizki.nbl123@gmail.com>
2021-07-27 22:00:26 +0700

    new component for settings

---
+++ apps/src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="sidebar">
+      <ul>
+         <li><router-link to="/Settings">Edit Profile</router-link></li>
+         <li><router-link to="/Settings/Password">Change Password</router-link></li>
+         <li><router-link to="/Settings/DeleteAccount">Delete Account</router-link></li>
+      </ul>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "sidebar",
+};
+</script>
+
+<style scoped>
+* {
+   padding: 0;
+   margin: 0;
+   box-sizing: border-box;
+}
+
+.sidebar {
+   width: 339px;
+}
+
+.sidebar ul {
+   list-style: none;
+}
+</style>
---
+++ apps/src/components/TopBar.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="head">
+      <div class="profile-pic">
+         <img src="../../public/img/profile-default.svg" alt="" />
+      </div>
+      <h1 v-if="username">{{ username.data.name }} / Edit Profile</h1>
+   </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+   name: "topbar",
+   data() {
+      return {
+         username: null,
+      };
+   },
+   async created() {
+      // axios.get("api/v1/users/").then((Response) => (this.info = Response));
+      const response = await axios.get("api/v1/user");
+      this.username = response.data;
+   },
+};
+</script>
+
+<style scoped>
+.head {
+   display: flex;
+   height: 120px;
+}
+</style>
