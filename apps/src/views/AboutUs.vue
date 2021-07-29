0b06b91a8357053a1ecee29e9cafef3517734716
rizkinabil <rizki.nbl123@gmail.com>
2021-07-30 00:47:03 +0700

    update interface

--- apps/src/views/AboutUs.vue
+++ apps/src/views/AboutUs.vue
@@ -6,9 +6,68 @@
       <div class="main">
          <div class="card">
             <h1>About Us</h1>
-            <div class="body"></div>
+            <p class="headline">
+               fingerVote is a website poll created by Muhammad Ali Fikri, Rizki Nabil Aufa, and Ahmad Nur Rizal. the purpose of making this website is as an internship assignment from the RPLGDC laboratory. fingerVote website is free for
+               anyone who wants to use it, besides that the fingerVote website is very easy to use.
+            </p>
+            <div class="body">
+               <h2 class="team">Our Team</h2>
+               <div class="uiux">
+                  <div class="show">
+                     <div class="foto">
+                        <img src="../../public/img/vector/pp-ui.svg" alt="" />
+                     </div>
+                     <div class="desc">
+                        <h2>Muhammad Ali Fikri</h2>
+                        <p>UI/UX Designer</p>
+                     </div>
+                  </div>
+                  <p>
+                     Hallo...! <span><br /></span> My name is Muhammad Ali Fikri you can called me fikri. I’M currently studying at Department of informatics, I have an interest in UI/UX Designer. I think the process of making an
+                     application or website before entering the development process has its own excitement, where we need to research first, after knowing what user wants we will create the inter face.
+                  </p>
+               </div>
+               <div class="FE">
+                  <p>
+                     Hi, everyone! <span><br /></span> My name is Rizki Nabil Aufa. I’m a front-end developer. Focused in writing clean and user-friendly experience. Discipline and well organized person. I’m currently a computer science
+                     student and live in Bekasi, West Java, Indonesia. I have an interest in web development and data science. Trying my best to be a long life learner and professional.
+                  </p>
+                  <div class="show">
+                     <div class="foto">
+                        <img src="../../public/img/vector/pp-fe.svg" alt="" />
+                     </div>
+                     <div class="desc">
+                        <h2>Rizki Nabil Aufa</h2>
+                        <p>Front-end Developer</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="BE">
+                  <div class="show">
+                     <div class="foto">
+                        <img src="../../public/img/vector/pp-be.svg" alt="" />
+                     </div>
+                     <div class="desc">
+                        <h2>Ahmad Nur Rizal</h2>
+                        <p>Back-end Developer</p>
+                     </div>
+                  </div>
+                  <p>
+                     Hallo...! <span><br /></span> My name is Ahmad Nur Rizal you can called ical. Since entering the Department of Informatics I have been interested in knowing more about information technology, especially in the field of
+                     software engineering. Becoming a reliable back-end developer is my goal.
+                  </p>
+               </div>
+               <div class="information">
+                  <h2>General Information</h2>
+                  <p>
+                     FingerVote offers its user a free platform where they can create different types of polls. The philosophy why this website is named “fingerVote”, because we only need to use our finger to vote, we don’t need to come to
+                     the voting place, just by using gadget you can participate in voting. We built this website in 2021 on the assignment of the RPLGDC laboratory. We hope that fingerVote can help people to vote online.
+                  </p>
+               </div>
+            </div>
          </div>
       </div>
+      <Footer />
    </div>
 </template>
 
@@ -66,7 +125,74 @@ export default {
 }
 
 .card {
+   padding: 40px;
    width: 948px;
-   height: 623px;
+   height: 2360px;
+   background-image: linear-gradient(180deg, #aed8ff 0%, #3d87cc 100%);
+   margin: 100px auto;
+}
+
+.card h1 {
+   font-size: 48px;
+   margin-bottom: 30px;
+}
+
+.team {
+   font-size: 30px;
+   margin-bottom: 40px;
+}
+
+h1,
+h2 {
+   font-family: "Kanit", sans-serif;
+   color: #eaf5ff;
+}
+
+p {
+   font-family: "Roboto", sans-serif;
+   color: #eaf5ff;
+   font-size: 18px;
+   line-height: 32.4px;
+}
+
+.headline {
+   margin-bottom: 40px;
+}
+
+.uiux,
+.FE {
+   display: flex;
+   gap: 30px;
+   margin-bottom: 90px;
+}
+
+.BE {
+   display: flex;
+   gap: 30px;
+   margin-bottom: 40px;
+}
+
+.show {
+   background-color: #ffffff;
+   width: 340px;
+   height: 500px;
+   border-radius: 20px;
+}
+
+.foto {
+   padding: 20px;
+}
+
+.desc {
+   padding-top: 55px;
+   padding-bottom: 55px;
+   text-align: center;
+   background-color: #616161;
+   border-bottom-right-radius: 20px;
+   border-bottom-left-radius: 20px;
+}
+
+.information h2 {
+   margin-bottom: 30px;
 }
 </style>
--- apps/src/views/Contact.vue
+++ apps/src/views/Contact.vue
@@ -6,7 +6,27 @@
       <div class="main">
          <div class="card">
             <h1>Contact Us</h1>
-            <div class="body"></div>
+            <div class="body">
+               <p class="headline">If you want to ask something, you can contact the email below:</p>
+               <h3>UI/UX Designer</h3>
+               <div class="info">
+                  <p>Name : Muhammad Ali Fikri</p>
+                  <p>Email : fikriali48@gmail.com</p>
+               </div>
+               <h3>Front-end Developer</h3>
+               <div class="info">
+                  <p>Name : Rizki Nabil Aufa</p>
+                  <p>Email : rizki.nbl123@gmail.com</p>
+               </div>
+               <h3>Back-end Developer</h3>
+               <div class="info">
+                  <p>Name : Ahmad Nur Rizal</p>
+                  <p class="end">Email : ahmadnur.rizal45@gmail.com</p>
+               </div>
+               <div class="pic">
+                  <img src="../../public/img/contact-icon.svg" alt="" />
+               </div>
+            </div>
          </div>
       </div>
       <Footer />
@@ -67,7 +87,52 @@ export default {
 }
 
 .card {
+   margin: 100px auto;
+   padding: 30px 40px 65px;
    width: 948px;
    height: 623px;
+   background-image: linear-gradient(180deg, #aed8ff 0%, #3d87cc 100%);
+}
+
+.body {
+   position: relative;
+}
+
+.card h1 {
+   font-size: 48px;
+   margin-bottom: 35px;
+}
+
+.headline {
+   margin-bottom: 30px;
+}
+
+.info {
+   margin-bottom: 30px;
+}
+
+.info p {
+   margin-bottom: 10px;
+}
+p {
+   font-family: "Roboto", sans-serif;
+   color: #eaf5ff;
+   font-size: 18px;
+}
+.card h1 {
+   font-family: "Kanit", sans-serif;
+   color: #eaf5ff;
+}
+h3 {
+   font-size: 24px;
+   margin-bottom: 15px;
+   font-family: "Kanit", sans-serif;
+   color: #eaf5ff;
+}
+
+.pic {
+   position: absolute;
+   right: 0;
+   bottom: -25px;
 }
 </style>
