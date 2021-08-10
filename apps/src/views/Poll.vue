6b68b769b6f9e9551055be7efeac5df55f25301f
rizki nabil <rizki.nbl123@gmail.com>
2021-08-10 20:27:09 +0700

    add poll page

--- apps/src/views/Poll.vue
+++ apps/src/views/Poll.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="wrapper">
-    <h1> {{pollId}}</h1>
-
+    <h1>{{ pollId }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-          pollId: this.$route.params.id
-        }
-    }
-
-}
+  data() {
+    return {
+      pollId: this.$route.params.id,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
