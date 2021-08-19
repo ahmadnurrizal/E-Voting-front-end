40a89f2bb8b57b49e862112ca56569b72c1f6146
rizki nabil <rizki.nbl123@gmail.com>
2021-08-19 15:52:29 +0700

    add new component for polling

---
+++ apps/src/components/Headerpoll.vue
@@ -0,0 +1,183 @@
+<template>
+   <div class="outlayer">
+      <div class="head">
+         <h1>{{ title }}</h1>
+         <p>
+            Created by
+            <span style="color: #539BE0">{{ userdata.name }}</span> at
+            {{ created_at }}
+         </p>
+         <div class="toggle">
+            <img
+               src="../../public/img/polling-toggle.svg"
+               @click="toggleClick"
+               alt=""
+            />
+            <div class="dropDown" id="dropDown">
+               <a class="edit" href=""> <span>Edit Poll</span> </a>
+               <a class="reset" href=""> <span>Reset Poll</span> </a>
+               <a class="delete" href=""> <span>Delete Poll</span> </a>
+            </div>
+         </div>
+      </div>
+      <div class="desc">
+         <h2>"{{ description }}"</h2>
+         <img src="" alt="" />
+      </div>
+   </div>
+</template>
+
+<script>
+   import axios from "axios";
+   export default {
+      name: "Headerpoll",
+
+      data() {
+         return {
+            pollId: this.$route.params.id,
+            userdata: "",
+
+            title: "",
+            created_at: "",
+            description: "",
+         };
+      },
+      methods: {
+         toggleClick() {
+            document.getElementById("dropDown").classList.toggle("show");
+         },
+      },
+      async created() {
+         const header = {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+         };
+         const user = await axios.get("api/v1/user", {
+            headers: header,
+         });
+         this.userdata = user.data.data;
+         const response = await axios.get("api/v1/user-poll", {
+            headers: header,
+         });
+         let lengthPoll = response.data.message.length;
+         // matching id
+         for (let i = 0; i < lengthPoll; i++) {
+            if (this.pollId == response.data.message[i].id) {
+               this.idMatched = response.data.message[i].id;
+               let lastPoll = response.data.message[i];
+               (this.title = lastPoll.title),
+                  (this.created_at = lastPoll.created_at),
+                  (this.description = lastPoll.description);
+            }
+         }
+      },
+   };
+</script>
+
+<style scoped>
+   @import url("https://fonts.googleapis.com/css2?family=Kanit:wght@500;600&family=Roboto&display=swap");
+
+   .head h1,
+   .desc h2 {
+      font-family: "Kanit", sans-serif;
+      text-align: center;
+   }
+
+   .head h1 {
+      font-weight: 600;
+      font-size: 48px;
+      color: #eaf5ff;
+   }
+
+   .head {
+      position: relative;
+   }
+
+   .head p {
+      font-family: "Roboto", sans-serif;
+      font-weight: 400;
+      color: #eaf5ff;
+      font-size: 18px;
+      text-align: center;
+   }
+
+   .toggle {
+      display: inline-block;
+   }
+
+   .toggle img {
+      position: absolute;
+      top: 12px;
+      right: 12px;
+      cursor: pointer;
+   }
+
+   .dropDown {
+      display: none;
+      position: absolute;
+      background-color: #bde0ff;
+      border: none;
+      border-radius: 3px;
+      width: 119px;
+      height: 125px;
+      top: 50px;
+      right: 12px;
+      padding: 8px 5px;
+      visibility: hidden;
+      opacity: 0;
+   }
+
+   .dropDown a {
+      display: block;
+      text-decoration: none;
+      font-family: "Kanit", sans-serif;
+      font-weight: 400;
+      font-size: 16px;
+      color: #1e6599;
+   }
+
+   .dropDown a:hover:not(.delete) {
+      color: #fbfcfd;
+   }
+
+   .dropDown span {
+      margin-left: 20px;
+   }
+
+   .edit {
+      background-image: url("../../public/img/polling-edit-icon.svg");
+      background-repeat: no-repeat;
+      background-position: left;
+   }
+   .reset {
+      background-image: url("../../public/img/polling-reset-icon.svg");
+      background-repeat: no-repeat;
+      background-position: left;
+      margin: 11px auto;
+   }
+   .delete {
+      background-image: url("../../public/img/polling-delete-icon.svg");
+      background-repeat: no-repeat;
+      background-position: left;
+   }
+
+   .delete:hover {
+      color: red;
+   }
+
+   .show {
+      display: block;
+      visibility: visible;
+      opacity: 1;
+   }
+
+   .desc {
+      margin: 44px auto 120px;
+   }
+
+   .desc h2 {
+      font-weight: 500;
+      font-size: 24px;
+      color: #eaf5ff;
+      margin-bottom: 52px;
+   }
+</style>
---
+++ apps/src/components/Sharepoll.vue
@@ -0,0 +1,75 @@
+<template>
+   <div class="main">
+      <div class="card">
+         <h1>Share Poll</h1>
+         <div class="link-address">
+            <input type="text" placeholder="link yang mau dishare" />
+            <button>Copy Link</button>
+         </div>
+         <div class="socmed">
+            <a href="">instagram</a>
+            <a href="">telegram</a>
+            <a href="">twitter</a>
+            <a href="">facebook</a>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: "Sharepoll",
+   };
+</script>
+
+<style scoped>
+   @import url("https://fonts.googleapis.com/css2?family=Kanit:wght@500;600&family=Roboto&display=swap");
+
+   .card {
+      border: none;
+      border-radius: 10px;
+      box-shadow: 0 20px 26px rgba(54, 37, 37, 0.2);
+      background-image: linear-gradient(180deg, #aed8ff 0%, #3d87cc 100%);
+      width: 948px;
+      min-height: 290px;
+      margin: auto auto 100px;
+      padding: 30px auto 50px;
+      text-align: center;
+   }
+
+   .card h1,
+   button {
+      font-family: "Kanit", sans-serif;
+   }
+
+   h1 {
+      margin-bottom: 30px;
+      font-weight: 700;
+      color: #eaf5ff;
+   }
+
+   button {
+      font-weight: 500;
+      border: none;
+      border-radius: 25px;
+      background-color: #1e6599;
+      font-size: 18px;
+      color: #ffffff;
+      width: 180px;
+      height: 40px;
+      margin-left: 20px;
+   }
+
+   .link-address {
+      margin-bottom: 50px;
+      align-items: center;
+   }
+
+   .link-address input {
+      width: 280px;
+      height: 40px;
+      border: 1px solid #858484;
+      box-sizing: border-box;
+      border-radius: 5px;
+   }
+</style>
