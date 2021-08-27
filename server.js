f55075592f9d1c3ab73cfbf436ff24c9a8c85144
rizki nabil <rizki.nbl123@gmail.com>
2021-08-27 16:04:46 +0700

    config history rollback

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const history = require("connect-history-api-fallback");
+
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 
 const app = express();
+
+app.use(history());
 
 //here we are configuring dist to serve app files
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
